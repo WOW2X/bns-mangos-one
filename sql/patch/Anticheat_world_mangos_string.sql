c5b2f1aba1d85a8aa6e218d0f918a72e7badbd69
keyshuwen <285196897@qq.com>
2013-05-28 18:17:09 +0800

    添加缺少的SQL和配置文件..

---
+++ sql/patch/Anticheat_world_mangos_string.sql
@@ -0,0 +1,5 @@
+DELETE FROM `mangos_string` WHERE `entry`='11001';
+INSERT INTO `mangos_string`
+    (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`)
+VALUES
+    (11001, '|cffff0000[Anticheat]: Cheater detected. Nickname: %s. Cheat type: %s.|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '|cffff0000[均扶找我折我找]: 完我找快把 抉忌扶忘把批忪快扶. 妒技攸: %s. 妥我扭 折我找忘: %s.|r');
